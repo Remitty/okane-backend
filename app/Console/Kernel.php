fb41655c968a8798b276ea831f0bbe714140f87b
gitdevstar <danielwarhn.online@gmail.com>
2023-02-14 16:52:09 -0500

    change checkAccountCommand timer

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('6hourly:callbackcheck')->everySixHours()->runInBackground();
-        $schedule->command('3hourly:checkaccount')->everyMinute()->runInBackground();
+        $schedule->command('3hourly:checkaccount')->everyThreeHours()->runInBackground();
     }
 
     /**
